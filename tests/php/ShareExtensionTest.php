04392b40101de1d36d01841142115d6445786560
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2021-08-17 23:12:43 +0200

    test

---
+++ tests/php/ShareExtensionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Syntro\Silvershare\Tests;
+
+use SilverStripe\Dev\SapphireTest;
+
+/**
+ * Test the Share extension
+ *
+ * @author Matthias Leutenegger <hello@syntro.ch>
+ */
+class ShareExtensionTest extends SapphireTest
+{
+
+    /**
+     * testTest
+     *
+     * @return void
+     */
+    public function testTest()
+    {
+        $this->assertEquals(2, 1+1);
+    }
+}
