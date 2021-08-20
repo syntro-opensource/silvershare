4ea08533129ae9cd7cc34432a84691025860dd32
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2021-08-20 12:08:55 +0200

    test field creation in extension

---
+++ src/Dev/TestDataObject.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Syntro\SilverShare\Dev;
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Dev\TestOnly;
+use Syntro\SilverShare\Extension\ShareExtension;
+
+/**
+ * A test dataobject which allows the extension-test
+ *
+ * @author Matthias Leutenegger <hello@syntro.ch>
+ */
+class SharedObject extends DataObject implements TestOnly
+{
+
+    private static $sharing_allow_user_overwrite = true;
+
+    /**
+     * Database fields
+     * @var array
+     */
+    private static $db = [
+        'Title' => 'Varchar',
+        'Description' => 'Text'
+    ];
+
+    /**
+     * Defines extension names and parameters to be applied
+     *  to this object upon construction.
+     *  @var array
+     */
+    private static $extensions = [
+        ShareExtension::class,
+    ];
+}
--- tests/php/ShareExtensionTest.php
+++ tests/php/ShareExtensionTest.php
@@ -3,6 +3,7 @@
 namespace Syntro\Silvershare\Tests;
 
 use SilverStripe\Dev\SapphireTest;
+use Syntro\SilverShare\Dev\SharedObject;
 
 /**
  * Test the Share extension
@@ -13,12 +14,38 @@ class ShareExtensionTest extends SapphireTest
 {
 
     /**
-     * testTest
+     * Defines the fixture file to use for this test class
+     * @var string
+     */
+    protected static $fixture_file = './defaultfixture.yml';
+
+    /**
+     * testCMSFields
      *
      * @return void
      */
-    public function testTest()
+    public function testCMSFields()
     {
-        $this->assertEquals(2, 1+1);
+        $object = SharedObject::create();
+        $fields = $object->getCMSFields()->dataFieldNames();
+        $this->assertContains('OGImage', $fields);
+        $this->assertContains('OGTitle', $fields);
+        $this->assertContains('OGDescription', $fields);
+        $this->assertNotContains('OGType', $fields);
+        $this->assertNotContains('TwitterType', $fields);
+
+        $object->config()->set('sharing_available_og_types', ['website']);
+        $object->config()->set('sharing_available_twitter_types', ['summary']);
+        $fields = $object->getCMSFields()->dataFieldNames();
+        $this->assertContains('OGType', $fields);
+        $this->assertContains('TwitterType', $fields);
+
+        $object->config()->set('sharing_allow_user_overwrite', false);
+        $fields = $object->getCMSFields()->dataFieldNames();
+        $this->assertNotContains('OGImage', $fields);
+        $this->assertNotContains('OGTitle', $fields);
+        $this->assertNotContains('OGDescription', $fields);
+        $this->assertNotContains('OGType', $fields);
+        $this->assertNotContains('TwitterType', $fields);
     }
 }
