7241395019653f996233226f7fbb99d1f33174ef
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2021-08-20 12:31:16 +0200

    test siteconfig extension

---
+++ tests/php/ShareSiteConfigExtensionTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Syntro\Silvershare\Tests;
+
+use SilverStripe\Dev\SapphireTest;
+use Syntro\SilverShare\Dev\SharedObject;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\SiteConfig\SiteConfig;
+
+/**
+ * Test the Share SiteConfig extension
+ *
+ * @author Matthias Leutenegger <hello@syntro.ch>
+ */
+class ShareSiteConfigExtensionTest extends SapphireTest
+{
+
+    /**
+     * Defines the fixture file to use for this test class
+     * @var string
+     */
+    protected static $fixture_file = './defaultfixture.yml';
+
+    /**
+     * setUp - add a siteconfig if necessary
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = DataObject::get_one(SiteConfig::class);
+        if (!$config) {
+            SiteConfig::make_site_config();
+        }
+    }
+
+    /**
+     * testCMSFields
+     *
+     * @return void
+     */
+    public function testCMSFields()
+    {
+        $object = SiteConfig::current_site_config();
+        $fields = $object->getCMSFields()->dataFieldNames();
+        $this->assertContains('OGDefaultImage', $fields);
+        $this->assertContains('TwitterSite', $fields);
+    }
+}
