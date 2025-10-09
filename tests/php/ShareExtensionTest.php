e5fb3182bf491215a66b302267ce742ad7be4125
Patrick Côté <git@ptct.ch>
2025-10-09 14:25:33 +0200

    testing: see if there's ANYTHING in the config

--- tests/php/ShareExtensionTest.php
+++ tests/php/ShareExtensionTest.php
@@ -57,7 +57,7 @@ class ShareExtensionTest extends SapphireTest
      */
     public function testDescriptionFallback()
     {
-        throw new \Exception(var_dump(Config::getAll()), 1);
+        throw new \Exception(var_dump(Config::inst()->getAll()), 1);
 
         $object = SharedObject::create();
         // without description, falls back to function
