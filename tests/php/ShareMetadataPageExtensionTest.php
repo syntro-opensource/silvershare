14ba5620ef62e1a8da62a391c6a7efe1649c847f
Patrick Côté <git@ptct.ch>
2025-10-09 15:08:43 +0200

    testing: more testing

--- tests/php/ShareMetadataPageExtensionTest.php
+++ tests/php/ShareMetadataPageExtensionTest.php
@@ -45,6 +45,8 @@ class ShareMetadataPageExtensionTest extends SapphireTest
 
         $metatags = $sitetree->MetaComponents();
         $tagnames = array_keys($metatags);
+        throw new \Exception(var_dump($tagnames), 1);
+
         $this->assertContains('og:name', $tagnames);
         $this->assertContains('og:type', $tagnames);
         $this->assertContains('twitter:card', $tagnames);
